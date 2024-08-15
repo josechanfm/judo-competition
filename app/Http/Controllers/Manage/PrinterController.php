b16eaf3fa173c00a33f6baa0b07c8e3b7a3000f5
josechanfm <josechan@ipm.edu.mo>
2024-08-15 18:03:54 +0800

    mpdf round ractang

--- app/Http/Controllers/Manage/PrinterController.php
+++ app/Http/Controllers/Manage/PrinterController.php
@@ -8,6 +8,43 @@ use Illuminate\Http\Request;
 class PrinterController extends Controller
 {
     public function htmlToPdf(){
+        $data='<h3>Hellow World</h3>';
+        $mpdf = new \Mpdf\Mpdf();
+        $mpdf->AddPage();
+        $style = array('width' => 0.5, 'cap' => 'butt', 'join' => 'miter', 'dash' => '10,20,5,10', 'phase' => 10, 'color' => array(255, 0, 0));
+        $style2 = array('width' => 0.5, 'cap' => 'butt', 'join' => 'miter', 'dash' => 0, 'color' => array(255, 0, 0));
+        $style3 = array('width' => 1, 'cap' => 'round', 'join' => 'round', 'dash' => '2,10', 'color' => array(255, 0, 0));
+        $style4 = array('L' => 0,
+                        'T' => array('width' => 0.25, 'cap' => 'butt', 'join' => 'miter', 'dash' => '20,10', 'phase' => 10, 'color' => array(100, 100, 255)),
+                        'R' => array('width' => 0.50, 'cap' => 'round', 'join' => 'miter', 'dash' => 0, 'color' => array(50, 50, 127)),
+                        'B' => array('width' => 0.75, 'cap' => 'square', 'join' => 'miter', 'dash' => '30,10,5,10'));
+        $style5 = array('width' => 0.25, 'cap' => 'butt', 'join' => 'miter', 'dash' => 0, 'color' => array(0, 64, 128));
+        $style6 = array('width' => 0.5, 'cap' => 'butt', 'join' => 'miter', 'dash' => '10,10', 'color' => array(0, 128, 0));
+        $style7 = array('width' => 0.5, 'cap' => 'butt', 'join' => 'miter', 'dash' => 0, 'color' => array(255, 128, 0));
+
+        // Rect
+        $mpdf->Text(100, 4, 'Rectangle examples');
+        $mpdf->Rect(100, 10, 40, 20, 'DF', $style4, array(220, 220, 200));
+        $mpdf->Rect(145, 10, 40, 20, 'D', array('all' => $style3));
+
+        // Rounded rectangle
+        $mpdf->Text(5, 249, 'Rounded rectangle examples');
+        //$mpdf->SetLineStyle(array('width' => 0.5, 'cap' => 'butt', 'join' => 'miter', 'dash' => 0, 'color' => array(0, 0, 0)));
+        $mpdf->RoundedRect(5, 255, 40, 30, 3.50, '1111', 'DF');
+        $mpdf->RoundedRect(50, 255, 40, 30, 6.50, '1000');
+        $mpdf->RoundedRect(95, 255, 40, 30, 10.0, '1111', null, $style6);
+        $mpdf->RoundedRect(140, 255, 40, 30, 8.0, '0101', 'DF', $style6, array(200, 200, 200));
+
+        $mpdf->Output('myfile.pdf', 'I');
+
+    }
+    function _Arc($x1, $y1, $x2, $y2, $x3, $y3)
+    {
+        $h = $this->h;
+        $this->_out(sprintf('%.2F %.2F %.2F %.2F %.2F %.2F c ', $x1*$this->k, ($h-$y1)*$this->k,
+            $x2*$this->k, ($h-$y2)*$this->k, $x3*$this->k, ($h-$y3)*$this->k));
+    }
+    public function programs(){
         $rows=[
             [
                 'category'=>'CAT A',
@@ -140,96 +177,9 @@ class PrinterController extends Controller
             $data .='<td>'.$rightTable.'</td>';
             $data .='</tr></table>';
             $data .='<div class="spacer"></div>';
-            }
-        // $data .='<div>';
-        // $data .='<div class="arc"></div>';
-        // $data .='<span class="arc">B</span>';
-        // $data .='</div>';
-        //echo $data;
+        }
         $mpdf = new \Mpdf\Mpdf();
         $mpdf->WriteHTML($data);
         $mpdf->Output('myfile.pdf', 'I');
-    }
-
-    public function programs(){
-        $file=storage_path('template//CK_B4_StudentRegistrationForm.xlsx');
-        $reader = \PhpOffice\PhpSpreadsheet\IOFactory::createReader("Xlsx");
-        $spreadsheet = $reader->load($file);
-        $data=[
-            "B1"=>"2024",
-            "B2"=>"初一",
-            "B3"=>"A",
-            "B4"=>"13",
-            "B5"=>"陳輝民 陳輝民 陳輝民 陳輝民 陳輝民 陳輝民 陳輝民 陳輝民 陳輝民 陳輝民 陳輝民 陳輝民 陳輝民 陳輝民 陳輝民 陳輝民 陳輝民 陳輝民 陳輝民 陳輝民 陳輝民 陳輝民 陳輝民 陳輝民 陳輝民 陳輝民 陳輝民 陳輝民 陳輝民 陳輝民 陳輝民 陳輝民 陳輝民 陳輝民 陳輝民",
-            "B6"=>"Jose Chan",
-            "B7"=>"M",
-            "B8"=>"1970",
-            "C8"=>"07",
-            "D8"=>"18",
-            "B9"=>"中國",
-            "B22"=>"陳大文",
-            "C22"=>"父親",
-            "D22"=>"54",
-            "E22"=>"programmer",
-            "F22"=>"my address ....",
-            "G22"=>"63860836",
-            "B23"=>"",
-            "C23"=>"",
-            "D23"=>"",
-            "E23"=>"",
-            "F23"=>"",
-            "G23"=>"",
-            "B28"=>"YES",
-            "B32"=>"2024-03-20",
-        ];
-        $spreadsheet->getActiveSheet()->getPageMargins()->setTop(1);
-        $spreadsheet->getActiveSheet()->getPageMargins()->setBottom(0.5);
-        $spreadsheet->getActiveSheet()->getPageMargins()->setLeft(0.5);
-        $spreadsheet->getActiveSheet()->getPageMargins()->setRight(0.4);
-
-        $spreadsheet->getActiveSheet()->getPageSetup()->setPrintArea('A1:K27');
-        $spreadsheet->getActiveSheet()->getPageSetup()->setFitToWidth(1);
-        $spreadsheet->getActiveSheet()->getPageSetup()->setFitToHeight(1);
-        
-        
-
-        //$spreadsheet->getActiveSheet()->getPageMargins()->setFooter(0.5);
-        //$spreadsheet->getActiveSheet()->getHeaderFooter()->setFirstFooter('First footer');
-        // $spreadsheet->getActiveSheet()->getHeaderFooter()->setEvenFooter('abc');
-        // $spreadsheet->getActiveSheet()->getHeaderFooter()->setOddFooter('abc');
-        $drawing = new \PhpOffice\PhpSpreadsheet\Worksheet\Drawing();
-        $drawing->setName('Paid');
-        $drawing->setDescription('Paid');
-        $drawing->setPath(storage_path('template//P1A01.png')); /* put your path and image here */
-        $drawing->setCoordinates('K4');
-        $drawing->setWidth(150);
-        $drawing->setHeight(150);
-        //$drawing->setOffsetX(110);
-        //$drawing->setRotation(25);
-        //$drawing->getShadow()->setVisible(true);
-        //$drawing->getShadow()->setDirection(45);
-        $drawing->setWorksheet($spreadsheet->getActiveSheet());
-        $spreadsheet->getActiveSheet()->getCell('k4')->setValue('');
-    
-        $sheet1=$spreadsheet->getSheet(1);
-        foreach($data as $key=>$value){
-            $sheet1->getCell($key)->setValue($value);
-        }
-        
-       $writer = \PhpOffice\PhpSpreadsheet\IOFactory::createWriter($spreadsheet, 'Mpdf');
-       $pdfPath=storage_path('template//form1.pdf');
-       $writer->save($pdfPath);
-
-        // Preview the PDF
-        if (file_exists($pdfPath)) {
-            header('Content-Type: application/pdf');
-            header('Content-Disposition: inline; filename="form1.pdf"');
-            header('Content-Length: ' . filesize($pdfPath));
-            readfile($pdfPath);
-            exit;
-        } else {
-            echo "File not found.";
-        }
-
     }
 }

fe709f613f9c54ca1cbd45571c388857e5700053
josechanfm <josechan@ipm.edu.mo>
2024-08-30 19:25:29 +0800

    seeder and demo script

--- database/seeders/GameTypeSeeder.php
+++ database/seeders/GameTypeSeeder.php
@@ -16,6 +16,66 @@ class GameTypeSeeder extends Seeder
     {
         $data=[
             [
+                'name' => 'IJF',
+                'name_secondary' => 'IJF',
+                'code'=>'IJF',
+                'winner_plus'=>'0',
+                'language'=>'en',
+                'is_language_secondary_enabled'=>0,
+                'language'=>'en',
+                'game_categories'=>[
+                    [
+                        'name' => 'Senior and Junior',
+                        'name_secondary' => 'Untitled',
+                        'code' => 'SJ',
+                        'weights' => [
+                            "FW45-","FW52-","FW57-","FW63-","FW70-","FW78-","FW78+","FWOPEN",
+                            "MW60-","MW66-","MW73-","MW81-","MW90-","MW100-","MW100+","MWOPEN"
+                        ],
+                        'duration' => 240
+                    ],
+                    [
+                        'name' => 'Cadet under 18 years',
+                        'name_secondary' => 'Untitled',
+                        'code' => 'CU18',
+                        'weights' => [
+                            "FW40-","FW44-","FW48-","FW52-","FW57-","FW63-","FW70-","FW70+",
+                            "MW50-","MW55-","MW60-","MW66-","MW73-","MW81-","MW90-","MW90+"
+                        ],
+                        'duration' => 240
+                    ],
+                    [
+                        'name' => 'Senior and Junior Mixed',
+                        'name_secondary' => 'Untitled',
+                        'code' => 'SJM',
+                        'weights' => [
+                            "FW57-","FW70-","FW70+",
+                            "MW73-","MW90-","MW90+"
+                        ],
+                        'duration' => 240
+                    ],
+                    [
+                        'name' => 'Senior Mixed Olympic Games',
+                        'name_secondary' => 'Untitled',
+                        'code' => 'SMOG',
+                        'weights' => [
+                            "FW57-","FW70-","FW70+",
+                            "MW73-","MW90-","MW90+"
+                        ],
+                        'duration' => 240
+                    ],
+                    [
+                        'name' => 'Cadet Mixed',
+                        'name_secondary' => 'Untitled',
+                        'code' => 'CM',
+                        'weights' => [
+                            "FW48-","FW63-","FW63+",
+                            "MW60-","MW81-","MW81+"
+                        ],
+                        'duration' => 240
+                    ]
+                ]
+            ],[
                 'name' => '學界賽',
                 'name_secondary' => 'COMPEONATO SESCOLAR',
                 'code'=>'CEJ',
